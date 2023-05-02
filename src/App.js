34e84cba6aad5992ff0e4de2f216ea4954db3b1f
Ishaa <ishaa481999@gmail.com>
2023-05-02 13:21:50 +0530

    e-commerce frontend

--- src/App.js
+++ src/App.js
@@ -1,54 +1,88 @@
 
 import './App.css';
-const mobiles=[ 
-  {
-        "_id": "63e8f5b9918c8981a0097ea4",
-        "model": "OnePlus 9 5G",
-        "price": 45000,
-        "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-        "company": "Oneplus"
-    },
-    {
-        "_id": "63e8f5b9918c8981a0097ea5",
-        "model": "Iphone 13 mini",
-        "price": 150000,
-        "img": "https://shop.unicornstore.in/uploads/images/medium/9d6a85b55d277eb058f2a9c14de1ae84.png",
-        "company": "Apple"
-    },
-    {
-        "_id": "63e8f5b9918c8981a0097ea6",
-        "model": "Samsung s21 ultra",
-        "price": 85000,
-        "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-        "company": "Samsung"
-    },
-    {
-        "_id": "63e8f5b9918c8981a0097ea7",
-        "model": "Xiomi mi 11",
-        "price": 25000,
-        "img": "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-        "company": "Xiomi"
-    }
-];
+import { createContext, useContext, useEffect, useState } from 'react';
+const cartContext=createContext();
+// const mobiles=[ 
+//   {
+//         "_id": "63e8f5b9918c8981a0097ea4",
+//         "model": "OnePlus 9 5G",
+//         "price": 45000,
+//         "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+//         "company": "Oneplus"
+//     },
+//     {
+//         "_id": "63e8f5b9918c8981a0097ea5",
+//         "model": "Iphone 13 mini",
+//         "price": 150000,
+//         "img": "https://shop.unicornstore.in/uploads/images/medium/9d6a85b55d277eb058f2a9c14de1ae84.png",
+//         "company": "Apple"
+//     },
+//     {
+//         "_id": "63e8f5b9918c8981a0097ea6",
+//         "model": "Samsung s21 ultra",
+//         "price": 85000,
+//         "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
+//         "company": "Samsung"
+//     },
+//     {
+//         "_id": "63e8f5b9918c8981a0097ea7",
+//         "model": "Xiomi mi 11",
+//         "price": 25000,
+//         "img": "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
+//         "company": "Xiomi"
+//     }
+// ];
+const API='http://localhost:9090'
 function App() {
+const [mobiles,setMobiles]=useState([]);
+const [cart,setCart]=useState([]);
+
+useEffect(()=>{
+fetch(`${API}/mobile`)
+.then((data)=>data.json())
+.then((mobiles)=>setMobiles(mobiles));
+},[]);
+
+useEffect(()=>{
+  fetch(`${API}/cart`)
+  .then((data)=>data.json())
+  .then((cart)=>setCart(cart));
+  },[]);
+
+ const updateCart=({mobile,type})=>{
+fetch(`${API}/cart?type=${type}`,{
+  method:"PUT",
+  body:JSON.stringify(mobile),
+  headers:{
+    "Content-Type":"application/json",
+  },
+
+}).then((data)=>data.json()).then((cart)=>setCart(cart));
+ };
   return (
     <div className="App">
       <header className="App-header">
+        <cartContext.Provider value={[mobiles,cart,updateCart]}>
         <PhoneList/>
         <CartList/>
+        </cartContext.Provider>
       </header>
     </div>
   );
 }
 function PhoneList(){
+  const [mobiles]=useContext(cartContext);
   return(
     <div className="phone-list-container">
-      {mobiles.map((mobile)=><Phone mobile={mobile} key={mobile._id} />)}
+      {mobiles.map((mobile)=>(<Phone mobile={mobile} key={mobile._id} />))}
+      
     </div>
-  )
+  );
+  
 }
 
 function Phone({mobile}){
+  const[ , ,  updateCart]=useContext(cartContext);
 
   return(
     <div className="phone-container">
@@ -56,21 +90,25 @@ function Phone({mobile}){
     <h2 className="phone-model">{mobile.model} </h2>
     <p className="phone-price">{mobile.price}</p>
     <h4 className="phone-company">{mobile.company}</h4>
-    <button className="phone-add-to-cart">Add-to-cart</button>
+    <button className="phone-add-to-cart" onClick={()=>updateCart({mobile,type:"increment"})}>Add-to-cart</button>
     </div>
   );
 }
 function CartList(){
-
+  const [, cart]=useContext(cartContext);
+  useEffect(()=>{},[cart]);
+  const total=cart.reduce((acc,mobile)=>acc=acc+(mobile.price*mobile.qty),0)
   return(
     <div className="cart-list-container">
         <h1>Shopping List</h1>
-      {mobiles.map((mobile)=><CartItem mobile={mobile} key={mobile._id} />)}
+      {cart.map((mobile)=>(<CartItem mobile={mobile} key={mobile._id} />))}
+      <h1>Total:{total}</h1>
+      <button>Checkout</button>
     </div>
   );
 }
 function CartItem({mobile}){
-
+const [ , , updateCart]=useContext(cartContext);
 
   return(
     <div className="cart-item-container">
@@ -81,8 +119,9 @@ function CartItem({mobile}){
     <h2 className="cart-item-model">{mobile.model} </h2>
     <p className="cart-item-price">{mobile.price}</p>
     <h4 className="cart-item-company">{mobile.company}</h4>
-   <button>+</button>
-   <button>-</button></div>
+   <button onClick={()=>updateCart({mobile,type:"increment"})}>+</button>
+   <p>{mobile.qty}</p>
+   <button onClick={()=>updateCart({mobile,type:"decrement"})}>-</button></div>
     </div>
   );
 }
